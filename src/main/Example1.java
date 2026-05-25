bfcf69ff6c14aa7b85cd06f9aec2cbac56fa303c
Gaurav Kumar Singh <sahil887415@gmail.com>
2026-05-25 15:02:07 +0530

    this is my first java program about datatype and variables.

---
+++ src/main/Example1.java
@@ -0,0 +1,20 @@
+package main;
+// Datatype and typecasting.
+
+public class Example1 {
+    static void main() {
+        //comment
+        byte age = 25;
+        short year = 2024;
+        int population = 100000;
+        long distance = 100000000;
+        float weight = 68.5f;
+        double height = 178.6;
+        char grade = 'A';
+        char grade2 = 65;
+        boolean isStudent = true;
+
+
+
+    }
+}
--- src/programm1.java
+++
@@ -1,6 +0,0 @@
-class Programm1{
-    static void main() {
-        System.out.println("hello java!");
-
-    }
-}
