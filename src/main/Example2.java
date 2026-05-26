4435194a5cda55cdde091a1951d33924a3e8c1ff
Gaurav Kumar Singh <sahil887415@gmail.com>
2026-05-26 23:07:10 +0530

    this is my java program about variables and their scope.

--- src/main/Example2.java
+++ src/main/Example2.java
@@ -1,14 +1,19 @@
 package main;
 
 public class Example2 {
-    public static void main() {
-        int x = 5;// x has local scope
 
-        if (x == 5){ // block starts here
-            int y =10;//y has block scope within if statement
-            System.out.println(y);// y can be accessed here
-        }//block ends here
-        System.out.println(x);// x can be access here
-        //System.out.println(y);// y can be accessed here
-    }// x destroyed here
+    public static void main(String[] args){
+
+
+        int x = 5; // x has local scope
+
+        if (x == 5) { // block starts here
+            int y = 10; // block scope
+            System.out.println(y);
+        } // block ends here
+
+        System.out.println(x);
+
+        // System.out.println(y); //
+    }
 }
