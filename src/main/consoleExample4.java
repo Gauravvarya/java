9774e0587faaba89d6950b67a0483d9dc251fe4e
Gaurav Kumar Singh <sahil887415@gmail.com>
2026-06-03 23:20:14 +0530

    This is consoleExample4.java about variables.

---
+++ src/main/consoleExample4.java
@@ -0,0 +1,16 @@
+package main;
+
+public class consoleExample4 {
+    static void main() {
+
+                System.out.println("Welcome to Java Programming!");
+
+                String name = "Gaurav";
+                int age = 20;
+
+                System.out.println("Name: " + name);
+                System.out.println("Age: " + age);
+
+                System.out.println("Next year, age will be " + (age + 1));
+            }
+        }
