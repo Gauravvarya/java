52d5e2227f4d5609a09ba367b800bef067aafd03
Gaurav Kumar Singh <sahil887415@gmail.com>
2026-06-14 01:42:33 +0530

    Even and Odd no. program

---
+++ src/ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse {
+    static void main(String[] args) {
+        int x =11;
+        if(x>10){
+            System.out.println("x is grater than 10");
+        }
+        else{
+            System.out.println("x is less than 10");
+        }
+    }
+}
